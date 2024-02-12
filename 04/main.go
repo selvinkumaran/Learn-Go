9aa371e20cfbd042afe468bc1470221f7b0f2714
selvinkumaran <selvin.sellappan@kumaran.com>
2024-02-12 23:39:07 +0530

    beginer

---
+++ 04/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+
+	fmt.Println("hello go")
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/selvinkumaran/Learn-Go
 
 go 1.21.6
+
+require (
+	github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
+	github.com/GoesToEleven/puppy v1.3.0 // indirect
+)
